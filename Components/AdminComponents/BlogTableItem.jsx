5c0eb21ef09028fa95bd834b3493e6fdfdb9f25d
Nelson Vui Lim Bong <nelsonvuilimbong@Mac.home>
2025-06-01 09:06:41 +0800

    Update BlogTableItem.jsx

--- Components/AdminComponents/BlogTableItem.jsx
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -8,8 +8,10 @@ const BlogTableItem = ({image,projectName,developer,date,deleteBlog,mongoId}) =>
     <tr className='bg-white border-b'>
         <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap '>
             <Image width={40} height={40} alt='' src={image?image:assets.logo} />
-            <p>{developer?developer:"No author"}</p>
         </th>
+        <td className='px-6 py-4'>
+            <p>{developer?developer:"No author"}</p>
+        </td>
         <td className='px-6 py-4'>
             {projectName?projectName:"no name provided"}
         </td>
